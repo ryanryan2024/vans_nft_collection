e8d3ed41f73ce9e8d06bc41e12bc7cd459063a58
Ryan <76850473+ryanryan2024@users.noreply.github.com>
2022-08-31 18:31:27 -0700

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,9 +1,16 @@
 const Header = () => {
     return (
-        <div className="flex justify-center py-10 px-2">
-            <h1 className="font-sans font-extralight">
-                Explore the Vans NFT collection including four different style types.
-            </h1>
+        <div className="py-10 px-2">
+            <div className="flex justify-center">
+                <h1 className="font-sans font-extralight">
+                    Explore the Vans NFT collection including four different style types.
+                </h1>
+            </div>
+            <div className="flex justify-center pt-2">
+                <h1 className="font-sans font-extralight">
+                    Note: If you would like to exchange in a different currency, <a href="https://mvrnft.netlify.app/contact.html" className="underline">contact us.</a>
+                </h1>
+            </div>
         </div>
     )
 }
